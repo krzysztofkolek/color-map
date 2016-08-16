657294985e644ffb1598a514928511950fe736cd
krzysztof.kolek.praca@gmail.com <krzysztof.kolek.praca@gmail.com>
2016-08-16 21:56:09 +0200

    ImageTransform

--- ColorMap/DataStructure/ProcessedImageData.cs
+++ ColorMap/DataStructure/ProcessedImageData.cs
@@ -1,13 +1,15 @@
-﻿
-namespace ColorMap.DataStructure
+﻿namespace ColorMap.DataStructure
 {
+    using ColorMap.DataStructure.Graph;
+
     /// <summary>
-    /// 
+    /// Output object for the whole process
     /// </summary>
     public class ProcessedImageData
     {
         public string Path { get; set; }
         public byte[,] RawImage { get; set; }
         public byte[,] BlackWhiteImage { get; set; }
+        public Graph<byte> ReadyGraph { get; set; }
     }
 }
