c48a9cee55cb693b879a8c388bdef66bf5f9f424
krzysztof.kolek.praca@gmail.com <krzysztof.kolek.praca@gmail.com>
2016-08-16 21:57:14 +0200

    Graph

---
+++ ColorMap/DataStructure/Graph/Graph.cs
@@ -0,0 +1,25 @@
+﻿namespace ColorMap.DataStructure.Graph
+{
+    using System.Collections.Generic;
+
+    public class Graph<T>
+    {
+        private List<Vertex<T>> _vertices { get; set; }
+        private bool[][] _edges;
+
+        public Graph()
+        {
+
+        }
+
+        public Graph(bool[][] edges)
+        {
+            _edges = edges;
+        }
+
+        public Graph<T> Build()
+        {
+
+        }
+    }
+}
---
+++ ColorMap/DataStructure/Graph/GraphFromArrayData.cs
@@ -0,0 +1,15 @@
+﻿namespace ColorMap.DataStructure.Graph
+{
+    using ColorMap.DataStructure.FloodFill;
+
+    public class GraphFromArrayData
+    {
+        public QueueFloodFillData InputData { get; set; }
+        public byte[,] BinaryArray { get; set; }
+
+        public GraphFromArrayData(QueueFloodFillData input)
+        {
+            InputData = input;
+        }
+    }
+}
